3ab764c61632d115764cf4d8df1f71a01700cf88
Kade <wangkai@youxin.cloud>
2022-06-24 10:45:18 +0800

    userService done

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -3,9 +3,10 @@ package service
 import (
 	"chat-room/internal/DO"
 	"chat-room/internal/dao/pool"
+	"chat-room/pkg/common/request"
+	"chat-room/pkg/common/response"
 	"chat-room/pkg/errors"
 	"chat-room/pkg/global/log"
-	"chat-room/pkg/common/response"
 	"time"
 
 	"github.com/google/uuid"
@@ -83,6 +84,56 @@ func (u *userService) GetUserOrGroupByName(name string) response.SearchResponse 
 		Group: *queryGroup,
 	}
 	return search
+}
+
+func (u *userService) GetUserList(uuid string) []DO.User {
+	db := pool.GetDB()
+
+	var queryUser *DO.User
+	db.First(&queryUser, "uuid = ?", uuid)
+	if NULL_ID == queryUser.Id {
+		return nil
+	}
+
+	var queryUsers []DO.User
+	db.Raw("SELECT DISTINCT u.username, u.uuid, u.avatar, u.nickname FROM user_friends uf LEFT JOIN users u ON uf.friend_id = u.id OR uf.user_id = u.id WHERE u.id != ? AND (friend_id = ? OR user_id = ?)", queryUser.Id, queryUser.Id, queryUser.Id).Scan(&queryUsers)
+	log.Logger.Debug("Get UserList-- queryUser", log.Any("queryUser", queryUser))
+	return queryUsers
+}
+
+func (u *userService) AddFriend(userFriendRequest *request.FriendRequest) error {
+	var queryUser *DO.User
+	db := pool.GetDB()
+	db.First(&queryUser, "uuid = ?", userFriendRequest.Uuid)
+	if NULL_ID == queryUser.Id {
+		log.Logger.Error("AddFriend-- queryUser", log.Any("queryUser", queryUser))
+		return errors.New("User not exists")
+	}
+
+	var friend *DO.User
+	db.First(&friend, "username = ?", userFriendRequest.FriendUsername)
+	if NULL_ID == queryUser.Id {
+		log.Logger.Error("ModifyUser-- queryUser", log.Any("queryUser", queryUser))
+		return errors.New("friend not found")
+	}
+
+	userFriend := DO.UserFriend {
+		UserId: queryUser.Id,
+		FriendId: friend.Id,
+	}
+
+	var userFriendQuery * DO.UserFriend
+	db.First(&userFriendQuery, "user_id = ? and friend_id = ?", queryUser.Id, friend.Id)
+	if userFriendQuery.ID != NULL_ID {
+		log.Logger.Info("ModifyUser-- queryUser", log.Any("queryUser", queryUser))
+		return errors.New("already your friend")
+	}
+
+	db.AutoMigrate(&userFriend)
+	db.Save(&userFriend)
+	log.Logger.Debug("userFriend", log.Any("userFriend", userFriend))
+	
+	return nil
 }
 
 func (u *userService) ModifyUserAvatar(avatar string, uuid string) error {
